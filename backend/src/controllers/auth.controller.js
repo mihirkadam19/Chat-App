d1393c5a514bcd70e4d521f16b36fbd98277a951
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-30 15:51:40 -0500

    all fields msg

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,6 +5,10 @@ import bcrypt from "bcryptjs"
 export const signup = async (req,res) => {
     const {fullName, email, password} = req.body;
     try{
+        if (!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"})
+        }
+
         if (password.length < 8){
             return res.status(400).json({message:"Password should be more than 8 characters"});
         }
