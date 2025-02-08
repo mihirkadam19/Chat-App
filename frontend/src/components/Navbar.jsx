48c0190f2c06b52a06af1241b0cee47a162f4a5b
mihirkadam19 <mihirkadam85@yahoo.in>
2025-02-08 10:30:30 -0500

    path re-assigned

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -24,14 +24,14 @@ const Navbar = () => {
 
           <div className="flex items-center gap-2">
             <Link
-              to={"/settings"}
+              to={"/themes"}
               className={`
               btn btn-sm gap-2 transition-colors
               
               `}
             >
               <Settings className="w-4 h-4" />
-              <span className="hidden sm:inline">Settings</span>
+              <span className="hidden sm:inline">Themes</span>
             </Link>
 
             {authUser && (
